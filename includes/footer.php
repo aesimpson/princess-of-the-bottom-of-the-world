0b79f5610733bebb9fd9884baea0f34ac6431ca1
Amy Simpson <asimpson@salon.com>
2019-02-24 00:04:10 -0800

    Delete footer.php

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-		<script type="text/javascript" src="<?php echo $base_url.'js/simple-scrollbar.js'; ?>"></script>
-		<script type="text/javascript" src="<?php echo $base_url.'js/jquery-3.3.1.min.js'; ?>"></script>
-		<script type="text/javascript" src="<?php echo $base_url.'js/app.js'; ?>"></script>
-	</body>
-</html>
