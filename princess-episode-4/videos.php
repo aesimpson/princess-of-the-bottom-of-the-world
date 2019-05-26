ae546e82923d868fdc7969664a188b32cdf3bf54
Amy Simpson <aesimpson16@gmail.com>
2019-05-26 10:54:29 -0700

    ep4 harbor video

--- princess-episode-4/videos.php
+++ princess-episode-4/videos.php
@@ -10,11 +10,51 @@
 				<div class="row justify-content-center">
 					<div class="col-sm-12 col-md-10 col-xl-8 p-4">
 						<div class="media-block p-4 my-4">
-							<video width="320" controls>
+							<video class="large" width="640" controls>
 								<source src="<?php echo $base_url.'videos/episode4/e4-penguin-stealing.mp4'; ?>" type="video/mp4">
 							</video>
 							<p class="caption mt-4">
 							Cuverville Island, Antarctica (2007) — A gentoo penguin collects rocks to strengthen a nest with its mate and two chicks inside. This heartwarming act of parental support comes with some rather suspicious behavior.
+							</p>
+						</div>
+						<div class="media-block p-4 my-4">
+							<video width="320" controls>
+								<source src="<?php echo $base_url.'videos/episode4/e4-penguin-snowcone.mp4'; ?>" type="video/mp4">
+							</video>
+							<p class="caption mt-4">
+							TBD
+							</p>
+						</div>
+						<div class="media-block p-4 my-4">
+							<video class="large" width="640" controls>
+								<source src="<?php echo $base_url.'videos/episode4/e4-penguin-highway.mp4'; ?>" type="video/mp4">
+							</video>
+							<p class="caption mt-4">
+							TBD
+							</p>
+						</div>
+						<div class="media-block p-4 my-4">
+							<video width="320" controls>
+								<source src="<?php echo $base_url.'videos/episode4/e4-neko-harbour.mp4'; ?>" type="video/mp4">
+							</video>
+							<p class="caption mt-4">
+							TBD
+							</p>
+						</div>
+						<div class="media-block p-4 my-4">
+							<video class="large" width="640" controls>
+								<source src="<?php echo $base_url.'videos/episode4/e4-penguin-highway.mp4'; ?>" type="video/mp4">
+							</video>
+							<p class="caption mt-4">
+							TBD
+							</p>
+						</div>
+						<div class="media-block p-4 my-4">
+							<video width="320" controls>
+								<source src="<?php echo $base_url.'videos/episode4/e4-drake-passage.mp4'; ?>" type="video/mp4">
+							</video>
+							<p class="caption mt-4">
+							TBD
 							</p>
 						</div>
                     </div>
