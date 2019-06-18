3225abe76e4bb87fdf30b4233faf4d586b2639e9
Amy Simpson <aesimpson16@gmail.com>
2019-06-17 18:48:38 -0700

    Adding in alt tags to episode 5

--- princess-episode-5/photos.php
+++ princess-episode-5/photos.php
@@ -10,31 +10,31 @@
 				<div class="row justify-content-center">
 					<div class="col-sm-12 col-md-10 col-xl-8 p-4">
                     <div class="media-block p-4 my-4">
-                            <img src="<?php echo $base_url.'images/episode5/e5-xx-tierra-guanacos.png'; ?>" alt="" />
+                            <img src="<?php echo $base_url.'images/episode5/e5-xx-tierra-guanacos.png'; ?>" alt="Guanacos grazing in field" />
 							<p class="caption mt-4">
                             <b>Tierra del Fuego, Argentina (2007) &mdash; </b>Distant relatives of camels, guanacos are one of four similar-looking species that include llamas, alpacas, and vicuñas. Grazers traveling in small herds, guanacos roam throughout Patagonia. Though one of the largest terrestrial mammals in South America, they still fall prey to natural predators, such as foxes and pumas. (Photo by Dan Linehan)
 							</p>
                         </div>
 						<div class="media-block p-4 my-4">
-                            <img src="<?php echo $base_url.'images/episode5/e5-xx-perito-calving.png'; ?>" alt="" />
+                            <img src="<?php echo $base_url.'images/episode5/e5-xx-perito-calving.png'; ?>" alt="A part of a glacier falling into the ocean" />
 							<p class="caption mt-4">
                            <b>El Calafate, Argentina (2007) &mdash; </b>The face of the Perito Moreno Glacier, in Patagonia, crumbles. (Photo by Dan Linehan)
 							</p>
                         </div>
 						<div class="media-block p-4 my-4">
-                            <img src="<?php echo $base_url.'images/episode5/e5-x0-iguazu-falls.png'; ?>" alt="" />
+                            <img src="<?php echo $base_url.'images/episode5/e5-x0-iguazu-falls.png'; ?>" alt="Iguazú Falls waterfall" />
 							<p class="caption mt-4">
                            <b>Iguazú, Argentina (2007) &mdash; </b> Along a subtropical jungle bordering Argentina and Brazil, Iguazú Falls contains a seemingly endless number of individual waterfalls, making it one of the world’s largest waterfall systems. (Photo by Dan Linehan)
 							</p>
                         </div>
 						<div class="media-block p-4 my-4">
-                            <img src="<?php echo $base_url.'images/episode5/e5-xx-caiman-pesos.png'; ?>" alt="" />
+                            <img src="<?php echo $base_url.'images/episode5/e5-xx-caiman-pesos.png'; ?>" alt="Caiman sitting in water" />
 							<p class="caption mt-4">
                             <b>Iguazú, Argentina (2007) &mdash; </b>I took a picture of this caiman encircled by pesos, but I am not going to show it. There was an incident. As I photographed the caiman in the still water above Iguazú Falls, a bunch of knuckleheads started throwing pesos. I couldn’t believe it. I was outraged. I wanted to throw them in. But the caiman was too small to eat them and probably would have been scared off. That wouldn’t have done any good. So, I just yelled at them until they stopped and left. (Photo by Dan Linehan)
 							</p>
                         </div>
 						<div class="media-block p-4 my-4">
-                            <img src="<?php echo $base_url.'images/episode5/e5-xx-dan-vine.png'; ?>" alt="" />
+                            <img src="<?php echo $base_url.'images/episode5/e5-xx-dan-vine.png'; ?>" alt="Dan Linehan swinging from a vine in the forest" />
 							<p class="caption mt-4">
                             <b>Iguazú, Argentina (2007) &mdash; </b>Me llamo Tarzan. Even if you don’t speak Spanish, does this really need a translation? In the subtropical jungle surrounding Iguazú Falls, traveling tree to tree by vine was much harder than it looks. But, I will add this: after falling on my ass, I learned that it was easier to swing from the thinner vine. (Photo courtesy of Dan Linehan)
 							</p>
