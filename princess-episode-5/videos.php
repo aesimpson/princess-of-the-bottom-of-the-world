0bb95a3849ed2862c0de9ffec8bfc0a5f129f7a2
Amy Simpson <aesimpson16@gmail.com>
2019-07-08 19:13:46 -0700

    changing episode 5 videos to youtube embeds

--- princess-episode-5/videos.php
+++ princess-episode-5/videos.php
@@ -10,43 +10,27 @@
 				<div class="row justify-content-center">
 					<div class="col-sm-12 col-md-10 col-xl-8 p-4">
 						<div class="media-block p-4 my-4">
-							<video class="large" width="640" controls>
-								<source src="<?php echo $base_url.'videos/episode5/e5-perito-moreno.mp4'; ?>" type="video/mp4">
-							</video>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/LvnEyhcgp2k" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 							<p class="caption mt-4">
-                            TBD
+                           <b>El Calafate, Argentina (2007) — </b> The ice of the Perito Moreno Glacier took centuries, maybe even millennia, to slowly turn from snowflakes into glacier ice crystals. The amount of change that occurs during this transformation is essentially imperceptible over the course of a human’s lifetime. Yet it only takes a matter of seconds for giant sections of ice, which are large enough to dwarf nearby sightseeing boats, to vanish.
 							</p>
 						</div>
 						<div class="media-block p-4 my-4">
-							<video class="large" width="640" controls>
-								<source src="<?php echo $base_url.'videos/episode5/e5-perito-moreno.mp4'; ?>" type="video/mp4">
-							</video>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/ONIdzLKdgDE" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 							<p class="caption mt-4">
-                            TBD
+                           <b>Iguazú, Argentina (2007) — </b> Iguazú Falls stretches for 1.7 miles on a rather circuitous route—shaped like a wildly deformed horseshoe—between the shorelines of Argentina and Brazil. On the Argentine side, the waterfalls penetrate more deeply into the jungle.
 							</p>
 						</div>
 						<div class="media-block p-4 my-4">
-							<video class="large" width="640" controls>
-								<source src="<?php echo $base_url.'videos/episode5/e5-iguazu-falls.mp4'; ?>" type="video/mp4">
-							</video>
-							<p class="caption mt-4">
-                            TBD
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/3ziLKpVznGc" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+								<p class="caption mt-4">
+                          <b>Iguazú, Argentina (2007) — </b> The butterflies at Iguazú National Park simply outshine all the surrounding flowers. Their vividness and delicacy is a tremendous balance to the power and grandeur of Iguazú Falls.
 							</p>
 						</div>
 						<div class="media-block p-4 my-4">
-							<video class="large" width="640" controls>
-								<source src="<?php echo $base_url.'videos/episode5/e5-jungle-butterflies.mp4'; ?>" type="video/mp4">
-							</video>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/EnDT_AkaG84" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 							<p class="caption mt-4">
-                            TBD
-							</p>
-						</div>
-						<div class="media-block p-4 my-4">
-							<video width="320" controls>
-								<source src="<?php echo $base_url.'videos/episode5/e5-rolling-swarm.mp4'; ?>" type="video/mp4">
-							</video>
-							<p class="caption mt-4">
-                            TBD
+                         	<b>Iguazú, Argentina (2007) — </b> This blob is a mass of caterpillars called a rolling swarm. While it may at first seem like they are using biomimicry to appear bigger than lowly caterpillar size, they group this way, astonishingly, for speed. Ordinarily, piling up a mass of organisms is most likely to slow things down. But these caterpillars have found a way to make their own moving walkway, like you find at an airport. Since the bottom layers of caterpillars are moving forward, any caterpillars on top will pick up a boost of speed. By continually cycling through top and bottom positions, this rolling swarm can move more than twice as fast as a single caterpillar.
 							</p>
 						</div>
                     </div>
